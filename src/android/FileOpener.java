d5ad61e0044f13c3f169aa04001917fd128cec01
davidngv <davidngv@yahoo.com>
2014-01-12 18:36:44 +0700

    More updates

--- src/android/FileOpener.java
+++ src/android/FileOpener.java
@@ -8,12 +8,10 @@
 
 package org.apache.cordova.plugins;
 
-/*
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-*/
 
 import android.content.Context;
 import android.content.Intent;
